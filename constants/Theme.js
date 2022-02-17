f34b1b131ae649ae5635e2864f4b258c4ef60696
Brandan King <brandan_king1998@outlook.com>
2022-02-17 14:01:45 +0000

    Initial setup of folder structure & files

---
+++ constants/Theme.js
@@ -0,0 +1,12 @@
+import { extendTheme, themeTools } from 'native-base';
+const themeConfig = {
+	colors: {},
+	config: {
+		initialColorMode: 'light',
+	},
+	components: {},
+};
+
+const Theme = extendTheme(themeConfig);
+
+export default Theme;
